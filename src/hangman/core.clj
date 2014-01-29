d21c7f4e92961255c6e966a6002b075aa20d7a63
Alan Thompson <thompson2526@gmail.com>
2014-01-28 16:47:50 -0800

    reading & parsing words.txt OK

--- src/hangman/core.clj
+++ src/hangman/core.clj
@@ -5,4 +5,14 @@
 
 
 (defn main []
-  (println "Hello, World!"))
+  (println "Hello, World!")
+  (let [ wordFile  (slurp "resources/words.txt")
+         ; _         (println "#1: " (take 25 wordFile))
+         lines     (str/split-lines wordFile)
+         ; _         (println "#2: " (take 5 lines))
+         words     (map str/trim lines)
+         ;_         (println "#3: " (take 5 (map #(str \" % \") words) ))
+       ] 
+    (println (take 5 (map #(str \" % \") words) ))
+  )
+)
