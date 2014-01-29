e30105e44367808d96b90c147f92db9403961b3a
Alan Thompson <thompson2526@gmail.com>
2014-01-28 17:04:19 -0800

    misc

--- src/hangman/core.clj
+++ src/hangman/core.clj
@@ -6,9 +6,9 @@
 
 (defn main []
   (println "Hello, World!")
-  (let [ lines (str/split-lines 
-                 (slurp"resources/words.txt") )
-         words  (map str/trim lines )
+  (let [ words   (->> (slurp"resources/words.txt")
+                      (str/split-lines )
+                      (map str/trim ) )
        ] 
     (println (take 5 (map #(str \" % \") words) ))
   )
