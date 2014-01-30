ac773310daa775246c4e8b40376bbb5a6fdc4fd7
Alan Thompson <thompson2526@gmail.com>
2014-01-30 11:09:39 -0800

    misc

--- src/hangman/core.clj
+++ src/hangman/core.clj
@@ -11,26 +11,25 @@
     (str seqName "("  (count seqVals) ") " )
       (take 5 (map #(str \" % \") seqVals) ) ))
 
-(defn words-of-len
-  "Finds words from a sequence with the specified length "
-  [word-seq target-len]
-  (filter #(= target-len (count %)) word-seq) )
-
 (def all-words   
+  "A collection of all words for the hangman game."
   (->> (slurp"resources/words.txt")
        (str/split-lines )
        (map str/trim ) ))
 
+(def len-word-map  
+  "Each key is a word length. Each value is a collection of all words of that length. Some
+  keys may be absent if no words of that length are present."
+  (group-by count all-words) )
+
+(def max-len 
+  "The maximum length of any word."
+  (apply max (keys len-word-map)) ) 
+
 (defn main []
   (println "main: enter")
   (show-info all-words "all-words")
-  (let [ 
-    max-len (apply max (map #(count %) all-words) ) 
-    map-by-size  (group-by count all-words)
-    ] 
-    (println "map-by-size:")
-    (doseq [ curr-len (range 1 (inc max-len)) ]
-      (let [ curr-words   (get map-by-size curr-len [] ) ]
-        (show-info curr-words (str "len=" curr-len) ) )
-    )
-  ))
+  (doseq [ curr-len (range 1 (inc max-len)) ]
+    (let [ curr-words   (get len-word-map curr-len [] ) ]
+      (show-info curr-words (str "len=" curr-len) ) )
+  ) )
